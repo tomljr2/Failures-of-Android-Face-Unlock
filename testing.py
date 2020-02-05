26b8331acaecdd1e0c7985000239ad2833086fca
Justin Tomlinson <justintomlinsonn@gmail.com>
2020-02-05 14:00:28 -0500

    Made lots of changes to incorporate data collection for physical testing
    Justin Tomlinson
    Feb 5, 2020

--- testing.py
+++ testing.py
@@ -1,16 +1,19 @@
-from records import records
+from digitalRecords import records
+from physicalRecords import records
 import os
 from os import listdir
 from os.path import isfile, join
 import numpy as np
 import cv2
 
-MONITOR_WIDTH=2560
-MONITOR_HEIGHT=1440
+MONITOR_WIDTH=2560.0
+MONITOR_HEIGHT=1440.0
+
+results=[]
 
 # Function to test images digitally. This will display each image and then
 # allow the user to input information about the person in the image.
-def digitalTesting():
+def digitalTesting(same=True):
    # Get the images
    os.system('cd utils;python3 extractFaces.py;cd ..')
    imgs = [f for f in listdir('utils/dst/') if isfile(join('utils/dst/', f))]
@@ -19,41 +22,73 @@ def digitalTesting():
    for img in imgs:
       image=cv2.imread('utils/dst/'+img)
       if image.shape[0] <= image.shape[1]:
-         w=MONITOR_WIDTH
+         w=int(MONITOR_WIDTH)
          h=int(image.shape[0] *(MONITOR_WIDTH/image.shape[1]))
       else:
          w=int(image.shape[1] *(MONITOR_HEIGHT/image.shape[0]))
-         h=MONITOR_HEIGHT
+         h=int(MONITOR_HEIGHT)
       image2=cv2.resize(image,(w,h))
       cv2.imshow(img, image2)
       cv2.waitKey(0)
       cv2.destroyAllWindows()
-      succ=-1
-      while succ > 10 or succ < 0:
-         succ=int(input('Number of successes: '))
-         if succ > 10 or succ < 0:
-            print('Error: Must be a number from 0 to 10')
+      recordResults('digital',img,same,image)
+
+def physicalTesting():
+   dists=['Close (Front Camera)','Medium (Front Camera)','Far (Front Camera)', \
+          'Close (Back Camera)','Medium (Back Camera)','Far (Back Camera)']
+   for d in dists:
+      print(d)
+      print()
+      recordResults('physical',None,True,None,d)
+   return
+
+# Record the results for different resolutions, distances, sexes,
+# ages, races, and nationalities.
+def recordResults(name,img,same,image=None,d=None):
+   os.system('cp '+name+'Records.py ./backup/'+name+'Backup.py')
+   f=open(name+'Records.py','w')
+
+   succ=int(input('Number of successes: '))
+   # If something was extracted as a face, but is wrong, then
+   # we can just enter an invalid number of successes to get
+   # rid of that picture
+   if succ > 10 or succ < 0:
+      if name=='digital':
+         os.system('rm utils/dst/'+img)
+      f.write('records='+str(records))
+      f.close()
+      return
+   if d == None:
       d=getDistance(image)
+
+   # If it's the same person, there's no need to repeat questions
+   if not same:
       s=getSex()
       a=getAge()
       r=getRace()
       print()
       n=input('Nationality: ')
-      recordResults(w,h,d,s,a,r,n,succ)
+   if not results and same:
+      results.append(getSex())
+      results.append(getAge())
+      results.append(getRace())
       print()
+      results.append(input('Nationality: '))
+   if same:
+      s=results[0]
+      a=results[1]
+      r=results[2]
+      n=results[3]
+   print()
 
-# Record the results for different resolutions, distances, sexes,
-# ages, races, and nationalities.
-def recordResults(w,h,dist,s,age,race,nat,succ):
-   os.system('cp records.py ./backup/backup.py')
-   f=open('records.py','w')
    addRecord(succ,'Totals')
-   addRecord(succ,(w,h,dist,s,age,race,nat))
-   addRecord(succ,dist)
+   addRecord(succ,(d,s,a,r,n))
+   addRecord(succ,d)
    addRecord(succ,s)
-   addRecord(succ,age)
-   addRecord(succ,race)
-   addRecord(succ,nat)
+   addRecord(succ,a)
+   addRecord(succ,r)
+   addRecord(succ,n)
+
    f.write('records='+str(records))
    f.close()
 
@@ -96,29 +131,49 @@ def getDistance(img):
    return dists[r-1]
 
 def getAge():
-   ages=['0 years','1-4 years','5-9 years','10-14 years','15-19 years', \
-         '20-24 years','25-29 years','30-34 years','35-39 years','40-44 years', \
-         '45-49 years','50-54 years','55-59 years','60-64 years','65-69 years', \
-         '70-74 years','75-79 years','80-84 years','85+ years']
+   ages=['0-4 years','5-9 years','10-14 years','15-19 years', \
+         '20-24 years','25-29 years','30-39 years','40-49 years', \
+         '50-59 years','60-69 years','70-79 years','80+ years']
    print()
-   for i in range(1,20):
+   for i in range(1,13):
       print(str(i)+'. '+ages[i-1])
    r=int(input('Age Group: '))
    return ages[r-1]
 
 # Function to predict the distance based on the resolution
 def predictDistance(img):
-   expected = np.array([2290,2770,2110,2550,1710,2065,1230, \
-                        1490,760,915,325,390])
-   h,w,c=img.shape
-   current = np.array([w,h,w,h,w,h,w,h,w,h,w,h])
-   res=np.subtract(expected,current)
-   A=([1,1,0,0,0,0,0,0,0,0,0,0],
-      [0,0,1,1,0,0,0,0,0,0,0,0],
-      [0,0,0,0,1,1,0,0,0,0,0,0],
-      [0,0,0,0,0,0,1,1,0,0,0,0],
-      [0,0,0,0,0,0,0,0,1,1,0,0],
-      [0,0,0,0,0,0,0,0,0,0,1,1])
-   res=np.matmul(A,res)
-   print('Expected choice: '+str(np.argmin(np.power(res,2))+1))
-digitalTesting()
+   try:
+      if img == None:
+         return
+   except:
+      expected = np.array([2290,2770,2110,2550,1710,2065,1230, \
+                           1490,760,915,325,390])
+      h,w,c=img.shape
+      current = np.array([w,h,w,h,w,h,w,h,w,h,w,h])
+      res=np.subtract(expected,current)
+      A=([1,1,0,0,0,0,0,0,0,0,0,0],
+         [0,0,1,1,0,0,0,0,0,0,0,0],
+         [0,0,0,0,1,1,0,0,0,0,0,0],
+         [0,0,0,0,0,0,1,1,0,0,0,0],
+         [0,0,0,0,0,0,0,0,1,1,0,0],
+         [0,0,0,0,0,0,0,0,0,0,1,1])
+      res=np.matmul(A,res)
+      print('Expected choice: '+str(np.argmin(np.power(res,2))+1))
+
+def mainMenu():
+   print('1. Digital Testing (Same person)')
+   print('2. Digital Testing (Multiple people)')
+   print('3. Physical Testing (Same person)')
+   print('4. Generate printables')
+   print('5. Quit')
+   x=int(input('Choice: '))
+   if x==1:
+      digitalTesting()
+   elif x==2:
+      digitalTesting(False)
+   elif x==3:
+      physicalTesting()
+   elif x==4:
+      os.system('cd utils;python3 physicalTesting.py;cd ..;')
+
+mainMenu()
---
+++ utils/physicalTesting.py
@@ -0,0 +1,61 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+import cv2
+os.system('python3 extractFaces.py;')
+
+HEIGHT=3264
+
+# Function to predict the distance based on the resolution
+def predictDistance(img):
+   try:
+      if img == None:
+         return
+   except:
+      expected = np.array([2290,2770,2110,2550,1710,2065,1230, \
+                           1490,760,915,325,390])
+      h,w,c=img.shape
+      current = np.array([w,h,w,h,w,h,w,h,w,h,w,h])
+      res=np.subtract(expected,current)
+      A=([1,1,0,0,0,0,0,0,0,0,0,0],
+         [0,0,1,1,0,0,0,0,0,0,0,0],
+         [0,0,0,0,1,1,0,0,0,0,0,0],
+         [0,0,0,0,0,0,1,1,0,0,0,0],
+         [0,0,0,0,0,0,0,0,1,1,0,0],
+         [0,0,0,0,0,0,0,0,0,0,1,1])
+      res=np.matmul(A,res)
+      return np.argmin(np.power(res,2))
+
+imgs = [f for f in listdir('dst/') if isfile(join('dst/', f))]
+
+distances=['Close(Front)','Medium(Front)','Far(Front)','Close(Back)', \
+           'Medium(Back)','Far(Back)']
+
+counter=1
+for i in range(0,len(imgs),2):
+   if i+1 < len(imgs):
+      img1=cv2.imread('dst/'+imgs[i])
+      t=distances[predictDistance(img1)]
+      factor = HEIGHT/img1.shape[0]
+      img1 = cv2.resize(img1,(0,0),fx=factor,fy=factor)
+      cv2.putText(img1,t,(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+      cv2.putText(img1,t,(10,70),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),2)
+      img2=cv2.imread('dst/'+imgs[i+1])
+      t=distances[predictDistance(img2)]
+      factor = HEIGHT/img2.shape[0]
+      img2 = cv2.resize(img2,(0,0),fx=factor,fy=factor)
+      cv2.putText(img2,t,(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+      cv2.putText(img2,t,(10,70),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),2)
+      printable=np.concatenate((img1,img2),axis=1)
+      cv2.imwrite('../printables/printable_'+str(counter)+'.jpg',printable)
+   else:
+      printable=cv2.imread('dst/'+imgs[i])
+      factor = HEIGHT/printable.shape[0]
+      t=distances[predictDistance(printable)]
+      printable = cv2.resize(printable,(0,0),fx=factor,fy=factor)
+      cv2.putText(printable,t,(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+      cv2.putText(printable,t,(10,70),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),2)
+      cv2.imwrite('../printables/printable_'+str(counter)+'.jpg',printable)
+   counter+=1
+
